2e946aa35ecc0728e84db98c8c432d407537fc08
Matthieu MICON <matthieu.micon4@gmail.com>
2026-02-16 11:30:41 +0100

    Etape6 - fonctionelleInsertion d'une oeuvre en BDD

---
+++ traitement.php
@@ -0,0 +1,24 @@
+<?php
+
+if(empty($_POST['titre']) 
+    || empty($_POST['description']) 
+    || empty($_POST['artiste']) 
+    || empty($_POST['image'])
+    || strlen($_POST['description']) < 3
+    || !filter_var($_POST['image'], FILTER_VALIDATE_URL)) {
+    header('Location: ajouter.php');
+} else {
+    $titre = htmlspecialchars($_POST['titre']);
+    $description = htmlspecialchars($_POST['description']);
+    $artiste = htmlspecialchars($_POST['artiste']);
+    $image = htmlspecialchars($_POST['image']);
+
+    //insertion oeuvre
+    include 'bdd.php'; 
+    $bdd = connexion();
+
+    $requete = $bdd->prepare('INSERT INTO oeuvres (titre, description, artiste, image) VALUES (?, ?, ?, ?)');
+    $requete->execute([$titre, $description, $artiste, $image]);
+
+    header('location: oeuvre.php?id=' . $bdd->lastInsertId());
+}
